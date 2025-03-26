2233dc3cbcf106d70299ca326e340111c32f3503
Tari Namikpoh <tarinamikpoh@gmail.com>
2025-03-26 22:13:55 +0100

    fixing image issuee

--- backend/app.ts
+++ backend/app.ts
@@ -29,7 +29,7 @@ app.use(express.json())
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(flash());
-app.use(express.static('public'))
+app.use(express.static('public/images'))
 
 //=============================== SESSION CONFIG
 app.use(
