50b54b4eddbdc4dfd35f987c1d5ce81909030037
Tari Namikpoh <tarinamikpoh@gmail.com>
2025-03-26 23:34:08 +0100

    pfp bugs

--- src/pages/useronly/AccountIndex.tsx
+++ src/pages/useronly/AccountIndex.tsx
@@ -336,7 +336,7 @@ const AccountIndex = () => {
             />
           </Link>
           <Link to={"./profile"}>
-            {image ? <img src={`https://electrobank-main.onrender.com/images/${image}`} alt="Avatar" className="h-24 w-24 rounded-md" /> : <FontAwesomeIcon
+            {image ? <img src={`https://electrobank-main.onrender.com/images/${image}`} alt="Avatar" className="h-[40%] w-20%] object-contain rounded-lg" /> : <FontAwesomeIcon
               icon={faUser}
               className="text-[#27173E] h-6 pl-6  max-mdPhone:pl-5 max-mdPhone:h-5"
             />}
--- src/pages/useronly/UserProfile.tsx
+++ src/pages/useronly/UserProfile.tsx
@@ -171,8 +171,8 @@ const UserProfile = () => {
       <h6>{message}</h6>
       <div className="body bg-slate-100 font-poppins pt-24 pb-24 px-6">
         <div className=" dfAc">
-          <picture className="p-2 rounded-full shadow-[0_0_15px_rgba(0,0,0,.1)] ">
-          <img src={image ? `https://electrobank-main.onrender.com/images/`+image : img} alt="Avatar" className="h-24 w-24" />           
+          <picture className="p-2 w-[30%] h-[30%] rounded-full shadow-[0_0_15px_rgba(0,0,0,.1)] ">
+          <img src={image ? `https://electrobank-main.onrender.com/images/${image}` : img} className="w-[100%] rounded-full" />
           </picture>
          <Link to={'../upload-picture'}><h2>Change profile picture</h2></Link>
         </div>
