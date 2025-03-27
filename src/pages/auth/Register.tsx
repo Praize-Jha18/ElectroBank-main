8dfa9de9b385041a690bd1c461bc78f745e0eabd
Tari Namikpoh <tarinamikpoh@gmail.com>
2025-03-27 09:13:39 +0100

    minor bugs

--- src/pages/auth/Register.tsx
+++ src/pages/auth/Register.tsx
@@ -77,7 +77,7 @@ const Register = () => {
     e.preventDefault();
     try {
       const toastId: Id = toast.info("Creating user...", {
-        autoClose: true,
+        autoClose: 10000,
         closeOnClick: false,
       });
       const response = await axios.post("https://electrobank-main.onrender.com/signup", formState, {
